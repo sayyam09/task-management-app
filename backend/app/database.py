8461f1d7e8c56c75c77890c873093f70f48fe523
sayyam09 <sayyamjain400@gmail.com>
2024-07-18 19:48:19 +0530

    change the sqlite db url

--- backend/app/database.py
+++ backend/app/database.py
@@ -15,7 +15,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 #from app.models import Task  # Import the Task model
 
-DATABASE_URL = 'sqlite:///./task_management.db'
+DATABASE_URL = 'sqlite:////tmp/task_management.db'
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
